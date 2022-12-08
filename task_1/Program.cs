64005f2a34b8f5b17c28c6de44416d4e25a4c8f6
FedJohn <ivan.fedyun@gmail.com>
2022-12-08 20:39:33 +0300

    Сделал исправления по комментариям

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace task_1
 {
@@ -11,36 +6,42 @@ namespace task_1
     {
         static void Main(string[] args)
         {
+            // Ф.И.О.
+            string fullName = "Иван Дмитриевич";
+            // Электронная почта
+            string email = "ivan.fedyun@gmail.com";
+            // Возраст
+            byte age = 17;
+            // Баллы по физике
+            int physicsPoints = 85;
+            // Баллы по математике
+            int mathPoints = 76;
+            // Баллы по програмированию
+            int progPoints = 97;
 
-            string fullName = "Иван Дмитриевич"; //Ф.И.О.
-            string email = "ivan.fedyun@gmail.com"; //Электронная почта
-            byte age = 17; //Возраст
-            int physics = 85; //Баллы по физике
-            int math = 76; //Баллы по математике
-            int programming = 97; //Баллы по програмированию
-
-            //Неудобный вариант
-            //string output = $"Ф.И.О.: {fullName} \nВозраст: {age} \nEmail: {email} \nБаллы по физике: {physics} \nБаллы по математике: {math} \nБаллы по програмированию: {programming}";
-            //Console.WriteLine(output);
-            //Console.ReadKey();
+            #region 
+            // Неудобный вариант
+            // string output = $"Ф.И.О.: {fullName} \nВозраст: {age} \nEmail: {email} \nБаллы по физике: {physics} \nБаллы по математике: {math} \nБаллы по програмированию: {programming}";
+            // Console.WriteLine(output);
+            // Console.ReadKey();
+            #endregion
 
             Console.WriteLine($"Ф.И.О.: {fullName}");
-            Console.WriteLine($"Email: {email}");   
+            Console.WriteLine($"Email: {email}");
             Console.WriteLine($"Возраст: {age}");
-            Console.WriteLine($"Баллы по физике: {physics}");
-            Console.WriteLine($"Баллы по математике: {math}");
-            Console.WriteLine($"Баллы по програмированию^ {programming}");
+            Console.WriteLine($"Баллы по физике: {physicsPoints}");
+            Console.WriteLine($"Баллы по математике: {mathPoints}");
+            Console.WriteLine($"Баллы по програмированию: {progPoints}");
             Console.ReadKey();
 
-            int sum; //Сумма баллов по всем предметам
-            sum = physics + math + programming;
-            Console.WriteLine($"\nСумма баллов: {sum}");
+            // Сумма баллов по всем предметам
+            int sum = physicsPoints + mathPoints + progPoints;
+            Console.WriteLine($"Сумма баллов: {sum}");
 
-            double average; //Средний балл
-            average = (physics + math + programming)/ 3;
+            //Средний балл
+            double average = (physicsPoints + mathPoints + progPoints) / 3;
             Console.WriteLine($"Средний балл: {average}");
             Console.ReadLine();
-             
         }
     }
 }
