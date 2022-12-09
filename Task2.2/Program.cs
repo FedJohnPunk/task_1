8b3d5fc2d78e7d03063bb2f6c8f35e318104f393
FedJohn <ivan.fedyun@gmail.com>
2022-12-09 23:55:11 +0300

    Добавил 1 и 2 пункт второго задания

---
+++ Task2.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Здравствуйте, укажите количество карт у вас в руках:");
+            int cardsAmount = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            Console.WriteLine("По очереди введите номинал каждой карты.(Возможные номиналы: 5, 6, 7, 8, 9, J, Q, K, T.)");
+            Console.WriteLine();
+            for (int counter = 0; counter < cardsAmount; counter++)
+            {
+                Console.WriteLine("Введите номинал карты, и нажмите Enter:");
+                var cardValue = Console.ReadLine();
+                bool cardCheck;
+                if (cardValue == "J" || cardValue == "Q" || cardValue == "K" || cardValue == "T")
+                {
+                    cardCheck = true;
+                }
+                else
+                {
+                    cardCheck = false;
+                }
+                if (cardCheck == true)
+                {
+                    sum = sum += 10;
+                }
+                else if (cardCheck == false)
+                {
+                    sum = sum + int.Parse(cardValue);
+                }
+            }                
+            Console.WriteLine($"Итоговая сумма карт: {sum}");
+            Console.WriteLine("Нажмите Enter для выхода");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пользователь вводит число для проверки
+            Console.WriteLine("Введите целое число: ");
+            int value = int.Parse(Console.ReadLine());
+
+            // Проверка числа с помощью деления с остатком
+            int valueCheck = value % 2;
+
+            if (valueCheck == 0)
+            {
+                Console.WriteLine("Данное число является чётным.");
+                Console.WriteLine("Нажмите Enter для выхода.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Данное число является нечётным.");
+                Console.WriteLine("Нажмите Enter для выхода.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
